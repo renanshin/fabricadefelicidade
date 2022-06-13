394d2d0c712955067fcb21c74eacbe4ad1165d78
renanshin <renanshin@gmail.com>
2022-06-13 19:13:40 -0300

    Commit inicial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Contato from './Components/Contato';
+
+function App() {
+  return (
+    
+    <div className="App">
+      <header className="App-header">
+        <Contato />
+      </header>
+    </div>
+  );
+}
+
+export default App;
